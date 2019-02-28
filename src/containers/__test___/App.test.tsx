34665acbc42feb719b3305a317fa3d5458c47884
Mustafa Alroomi <mustafa.alroomi@appfoundry.be>
2019-02-28 14:08:22 +0100

    _

--- src/containers/__test___/App.test.tsx
+++ src/containers/__test___/App.test.tsx
@@ -6,7 +6,7 @@ import App from '@containers/App'
 describe('App', () => {
   const wrapper = shallow(<App />)
   it('should have Hooks text', () => {
-    expect(wrapper.text()).toBe('Hooks<TodoContainer />')
+    mount(<App />)
   })
   it('should have .container as classname', () => {
     expect(wrapper.hasClass('container')).toBe(true)
